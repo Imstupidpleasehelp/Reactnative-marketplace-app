4e5a897b799e526a8f13817b6a5c3ef53c7524cc
Dylan <dylanabozarth@gmail.com>
2020-07-27 13:41:21 -0700

    lesson 1

--- App.js
+++ App.js
@@ -1,30 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert
+} from "react-native";
+//use safearea to make sure it's not covered
 export default function App() {
   //shake the phone for dev menu
-  console.log('It worked')
+  const HandlePress = () => alert('AAAA');
   return (
-    <View style={styles.container}>
-      <Text style={styles.whitetext}>We are published!
-
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    
+    <SafeAreaView style={styles.container}><Text style={styles.title}>Meme dictionary</Text>
+      {/*local image <Image styles={styles.image} source={require("./assets/download.png")} />*/}
+      <Text style={styles.whitetext}>DOGE </Text>
+      <View >
+        <Image
+          style={styles.image}
+          source={{
+            width: 200,
+            height: 300,
+            uri:
+              "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F000%2F640%2F866%2Fe59.png&f=1&nofb=1",
+          }}
+        />
+        {/* alerts */}
+        <Button title="click meh" onPress={() => Alert.alert('Do you', 'yes', [
+          {text: 'yes'},
+          {text: 'nah', onPress:HandlePress}
+        ])}  />
+      </View>
+    </SafeAreaView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'darkslategrey',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+    backgroundColor: "gold",
+    justifyContent: "center",
+    alignItems: "center",
+  
+  },
+  title: {
+justifyContent: "space-around",
+fontSize: 48,
+paddingLeft: 5,
+paddingRight: 5,
+backgroundColor: 'orange',
   },
   whitetext: {
-color: 'white',
-fontSize: 45
-  }
+    color: "black",
+    fontSize: 45,
+  },
+  image: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
