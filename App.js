6d61ea3f82bae51bf1bc1074f5a1f969f8156710
Dylan <dylanabozarth@gmail.com>
2020-07-27 22:00:23 -0700

    flexbox

--- App.js
+++ App.js
@@ -13,23 +13,25 @@ import {
   Button,
   Alert,
 } from "react-native";
-import {useDimensions,  useDeviceOrientation} from '@react-native-community/hooks' /* use this to support orientations */ 
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks"; /* use this to support orientations */
 //use safearea to make sure it's not covered
-
-
-
 
 export default function App() {
   //shake the phone for dev menu
- const {landscape} = useDeviceOrientation()
+  /* const {
+    landscape,
+  } = useDeviceOrientation(); /* change with orientation  <Text style={{color: landscape ? 'green' : 'black',}}>Meme dictionary</Text> */
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={{color: landscape ? 'green' : 'black',}}>Meme dictionary</Text>
-<View style={styles.box}><Text style={styles.whitetext}>REDaX</Text></View>
-{/* handling rotation */}
-      <View>
-      </View>
-    </SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: "blue", flex: 1, flexDirection: "row", paddingTop: 50, justifyContent: 'center', alignItems: "center" }}>
+      <View style={{ backgroundColor: "green", height: 100, width: 100 }} />
+      <View style={{ backgroundColor: "red", height: 100, width: 100 }} />
+      <View style={{ backgroundColor: "tomato", height: 100, width: 100 }} />
+      <View style={{ backgroundColor: "gold", height: 100, width: 100 }} />
+      
+    </SafeAreaView >
   );
 }
 
@@ -38,7 +40,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "gold",
     justifyContent: "center",
-    
+    alignItems: "center",
+    padding: 10,
   },
   title: {
     justifyContent: "space-around",
@@ -46,7 +49,6 @@ const styles = StyleSheet.create({
     paddingLeft: 5,
     paddingRight: 5,
     backgroundColor: "orange",
-    
   },
   whitetext: {
     color: "black",
@@ -57,9 +59,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   box: {
-backgroundColor: 'red',
-height: '50%',
-width: '60%',
+    backgroundColor: "red",
+    height: "50%",
+    width: "60%",
   },
 });
 
@@ -74,7 +76,8 @@ width: '60%',
   ])}  />*/
 }
 
-{/* web inmages <Image
+{
+  /* web inmages <Image
   style={styles.image}
   source={{
     width: 200,
@@ -83,4 +86,5 @@ width: '60%',
       "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi0.kym-cdn.com%2Fphotos%2Fimages%2Foriginal%2F000%2F640%2F866%2Fe59.png&f=1&nofb=1",
   }}
 />
-*/}
+*/
+}
