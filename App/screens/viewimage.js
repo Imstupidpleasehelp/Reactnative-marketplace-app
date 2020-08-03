415550c6ff4b56b0a2c71f316eeeb8d929e29fcd
Dylan <dylanabozarth@gmail.com>
2020-08-03 11:32:12 -0700

    done with styling

--- App/screens/viewimage.js
+++ App/screens/viewimage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  {View, Image, StyleSheet} from 'react-native'
+import  {View, Image, StyleSheet, Text, TouchableWithoutFeedback} from 'react-native'
 import colors from '../config/colors'
 import AppButton from '../components/appbutton';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
@@ -8,8 +8,13 @@ import {MaterialCommunityIcons} from '@expo/vector-icons';
 const ViewImageScreen = () =>  {
     return (
         <View style={styles.Container}>
-            <View style={styles.closeButton}></View>
-            <View style={styles.orderButton}></View>
+           
+            <View style={styles.closeButton}>
+                <MaterialCommunityIcons name="arrow-left" color="black" size={50} />
+            </View>
+            <View style={styles.orderButton}>
+                <MaterialCommunityIcons name="food-fork-drink" color="white" size={50} /><Text style={styles.order}>Order</Text>
+            </View>
 <Image resizeMode='contain' style={styles.Image} source={require('../assets/doge.jpg')} />
 <AppButton />
 </View>
@@ -22,27 +27,34 @@ backgroundColor: colors.secondary,
 flex: 1,
 paddingTop: 50
     },
+    topbar: {
+width: '100%',
+backgroundColor: colors.primary,
+top: 0
+    },
     closeButton: {
- height: 50,
- width: 50,
- backgroundColor: colors.primary,
+ 
  position: 'absolute',
  top: 35,
  left: 40,
     },
+    order: {
+color: 'white',
+fontSize: 18,
+alignItems: 'center',
+marginTop: -12
+    },
     orderButton: {
-        height: 50,
-        width: '100%',
-        backgroundColor: colors.forth,
-       position: 'absolute',
-        bottom: 0,
-        left: 40,  
-        marginBottom: 15
+        
+        
+        position: 'absolute',
+        top: 35,
+        right: 40,
     },
     Image: {
        width: 400,
-       height: 600,
-       marginTop: 45
-
+       height: 550,
+       marginTop: 55,
+marginBottom: 55
     }
 })
